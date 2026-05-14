d35f235734bd40849c2e8ee4d856fadea29a76a4
robin jack <robin10jack@gmail.com>
2026-05-14 02:37:36 +0200

    Decouple player lifecycle from game.gd into PlayerManager

---
+++ scripts/players/player_manager.gd
@@ -0,0 +1,101 @@
+class_name PlayerManager
+extends Node
+
+signal player_died(pid: int)
+
+const BOTINI_SCENE = preload("uid://cfxvxpnwen2eq")
+const BOSS_SCENE   = preload("uid://df6cjrmb84qw7")
+
+## peer_id -> character node
+var players: Dictionary = {}
+
+var _players_container: Node2D
+var _spawn_points: Array
+
+
+func setup(players_container: Node2D, spawn_points: Array) -> void:
+	_players_container = players_container
+	_spawn_points = spawn_points
+
+
+func spawn_warmup_player(pid: int, player_info: Dictionary) -> void:
+	if players.has(pid):
+		return
+
+	var player := BOTINI_SCENE.instantiate() as CharacterBody2D
+	player.peer_id     = pid
+	player.team        = pid
+	player.player_name = player_info.get("name", "Player %d" % pid)
+	player.name        = str(pid)
+
+	players[pid] = player
+	var sp: Node2D = _spawn_points[randi() % _spawn_points.size()]
+	player.position = sp.global_position - _players_container.global_position
+	_players_container.add_child(player)
+
+	var hc := player.get_node_or_null("Health") as Health
+	if hc:
+		hc.died.connect(func(): _on_player_died(pid))
+
+
+func spawn_round_players(player_infos: Dictionary, boss_peer_id: int, botinis_peer_ids: Array) -> void:
+	clear_players()
+
+	var botini_idx := 0
+	for pid: int in player_infos:
+		var is_boss := (pid == boss_peer_id)
+		var player: Player
+
+		if is_boss:
+			player              = BOSS_SCENE.instantiate()
+			player.peer_id      = pid
+			player.player_name  = player_infos[pid].get("name", "Botato %d" % pid)
+		else:
+			player              = BOTINI_SCENE.instantiate()
+			player.peer_id      = pid
+			player.player_name  = player_infos[pid].get("name", "Botini %d" % pid)
+
+		player.name = str(pid)
+		var sp: Node2D
+		if is_boss:
+			sp = _spawn_points[randi() % _spawn_points.size()]
+		else:
+			sp = _spawn_points[botini_idx % _spawn_points.size()]
+			botini_idx += 1
+		player.position = sp.global_position - _players_container.global_position
+		_players_container.add_child(player, true)
+		players[pid] = player
+
+		player.health.died.connect(func(): _on_player_died(pid))
+
+
+func respawn_warmup_player(pid: int) -> void:
+	var player_node: Player = players.get(pid)
+	if is_instance_valid(player_node):
+		var sp = _spawn_points[randi() % _spawn_points.size()]
+		player_node.respawn.rpc(sp.global_position)
+
+
+func remove_player(pid: int) -> void:
+	if players.has(pid):
+		var player_node: Player = players[pid]
+		if is_instance_valid(player_node):
+			if player_node.get_parent():
+				player_node.get_parent().remove_child(player_node)
+			player_node.queue_free()
+		players.erase(pid)
+
+
+func clear_players() -> void:
+	for c in _players_container.get_children():
+		_players_container.remove_child(c)
+		c.queue_free()
+	players.clear()
+
+
+func get_player(pid: int) -> Player:
+	return players.get(pid)
+
+
+func _on_player_died(pid: int) -> void:
+	player_died.emit(pid)
